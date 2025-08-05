9b0813c7da01c9e4662c7e2233191e3becb2995e
Noman Ali Virasat Ali Mir <mirnomanali.2001@gmail.com>
2025-08-05 20:57:34 +0530

    Implemented generic binary search to solve LeetCode 35 (Search Insert Position).
    If target not found, return 'lo' as the correct insert index. Learned how binary search
    divides index range, not array size, and how lo always lands on the right position.

--- Leet_code_Problems.py
+++ Leet_code_Problems.py
@@ -1,5 +1,5 @@
+# LeetCode problem 702 {Binary Search}
 """"
- leetCode problem 702 {Binary Search}
  Problem level: Easy
  Abstract: given an integer array which is sorted in ascending order need to find target from nums if its exist
  return its index otherwise return -1 {run time complexity 0(log n)}
@@ -38,4 +38,49 @@ binary_search does the usual loop stuff, and find_target just tells it when weâ€
 move. This way I can easily tweak it later for things like first/last occurrence, rotated arrays, 
 or any advanced version of binary search.
 And yeah, it's still O(log n) â€” fast and clean.
+"""
+
+#  LeetCode Problem: 35 { Search Insert Position }   Date: 04-08-2025
+
+"""
+ Difficulty level: Easy     
+ 
+ Abstract: We have to find the target if it's available in the array. The array is distinct. If not we have to  
+ calculate the correct position in sorted order of the target. And return the index where it would be if it were 
+ inserted in order.  
+ runtime complexity 0(log n)
+ 
+"""
+
+
+def b_s(lo, hi, condition):  # b_s = for binary search write just for better understanding and memorizing while solving
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return lo
+
+
+def searching_insert(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            return 'found'
+        elif nums[mid] < target:
+            return 'left'
+        else:
+            return 'right'
+    return b_s(0, len(nums) - 1, condition)
+
+
+"""
+    Here our b_s function is a generic Binary Search function which compare the conditions and find the midpoint of the
+    array and controls while loop. The second function Searching_insertion compare all the elements and checked if it's 
+    available in the array and if not then our generic function return the lo position of the array which is the right 
+    index of the given target. How so the Binary Search counts the index and divide it with and return the integer value
+    and compare with mid if it's small go towards right and compare it's big the answer is MIDPOINT.
 """
